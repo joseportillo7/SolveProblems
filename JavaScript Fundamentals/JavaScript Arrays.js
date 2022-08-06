602ac9e9543c4a90b9528a612a7972dca2b5d252
JosePortillo <jose.portillo.samayoa@gmail.com>
2022-08-05 20:28:40 -0600

    Problem #25 - Remove first n elements of an array

--- JavaScript Fundamentals/JavaScript Arrays.js
+++ JavaScript Fundamentals/JavaScript Arrays.js
@@ -6,6 +6,20 @@ const MyFunction1 = (a,n)=>{
     return a[n-1]
 }
 
+
+/**
+ * Write a function that takes an array (a) as argument
+ * Remove the first 3 element of a
+ * return the result
+ */
+const MyFunction2 = (a)=>{
+    return a.slice(3)
+}
+
+
+
+
 module.exports.jsArray = {
-    MyFunction1
+    MyFunction1,
+    MyFunction2
 }
--- TestJest/JavaScript Arryas.test.js
+++ TestJest/JavaScript Arryas.test.js
@@ -1,9 +1,27 @@
 const jsarray = require('../JavaScript Fundamentals/JavaScript Arrays')
 
-describe("JavaScriptArrays - Problem#24 - Get nth element of array",()=>{
+describe("JavaScriptArrays - Problem #24 - Get nth element of array",()=>{
     test("The result shoud be 3",()=>{
         const expected = 3
         const result = jsarray.jsArray.MyFunction1([1,2,3,4,5],3)
         expect(expected).toBe(result)
+    })
+    test("The result shoud be 6",()=>{
+        const expected = 6
+        const result = jsarray.jsArray.MyFunction1([10,9,8,7,6],5)
+        expect(expected).toBe(result)
+    })
+})
+
+describe('JavaScriptArrays - Problem #25 - Remove first n element of an array',()=>{
+    test('The result should be [4]',()=>{
+        const expected = [4]
+        const result = jsarray.jsArray.MyFunction2([1,2,3,4])
+        expect(expected).toEqual(result)
+    })
+    test('The result should be []',()=>{
+        const expected = []
+        const result = jsarray.jsArray.MyFunction2([1,2,3])
+        expect(expected).toEqual(result)
     })
 })
